ae727bf606e1a950b76ae4d6cb2114b6f75541ec
David McIntyre <david.v.mcintyre@gmail.com>
2021-10-28 17:38:41 +0100

    second name test passed

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,7 +3,13 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest = Guest("Commander Zavala")
+        self.guest1 = Guest("Commander Zavala")
+        self.guest2 = Guest("Ikora Rey")
+        self.guest3 = Guest("Saint-14")
+        self.guest4 = Guest("Master Chief")
 
-    def test_person_has_name(self):
-        self.assertEqual("Commander Zavala", self.guest.name)
+    def test_person_has_name1(self):
+        self.assertEqual("Commander Zavala", self.guest1.name)
+
+    def test_person_has_name2(self):
+        self.assertEqual("Ikora Rey", self.guest2.name)
